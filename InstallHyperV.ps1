911e90f8803d7e4cb93c123ed4a6c793d62dde34
Roden Hilario <rodjonh@gmail.com>
2021-02-18 08:30:52 +0000

    Edits

--- InstallHyperV.ps1
+++ InstallHyperV.ps1
@@ -9,3 +9,15 @@ Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Tools-All
 
 # Install the entire Hyper-V stack (hypervisor, services, and tools)
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
+
+# Install only the PowerShell module
+Install-WindowsFeature -Name Hyper-V-PowerShell
+
+# Install Hyper-V Manager and the PowerShell module (HVM only available on GUI systems)
+Install-WindowsFeature -Name RSAT-Hyper-V-Tools
+
+# Install the Hyper-V hypervisor and all tools (method #1)
+Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
+
+# Install the Hyper-V hypervisor and all tools (method #2)
+Install-WindowsFeature -Name Hyper-V, RSAT-Hyper-V-Tools
