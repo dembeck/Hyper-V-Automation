5633f3c7985007d22779ab01672bdd3a8a2b113d
Roden Hilario <rodjonh@gmail.com>
2021-02-23 05:09:13 +0000

    Edits to script

--- ConfigureNAT.ps1
+++ ConfigureNAT.ps1
@@ -3,6 +3,6 @@ New-NetNAT -Name “NATNetwork” -InternalIPInterfaceAddressPrefix 10.90.90.0/2
 
 New-NetIPAddress -InterfaceIndex 9 -IPAddress 10.90.90.1 -PrefixLength 24 -InterfaceAlias “NATSwitch”
 
-Get-NetNatStaticMapping
-Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/24" -ExternalPort 51005 -Protocol TCP -InternalIPAddress "10.90.90.5" -InternalPort 3389 -NatName NATNetwork
+#Get-NetNatStaticMapping
+#Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/24" -ExternalPort 51005 -Protocol TCP -InternalIPAddress "10.90.90.5" -InternalPort 3389 -NatName NATNetwork
 
