089a98b8b1cd9fedb0b418669dda80b4dc260ac5
Roden Hilario <rodjonh@gmail.com>
2021-02-23 12:34:56 +0000

    Edits to script

--- SetNetworking.ps1
+++ SetNetworking.ps1
@@ -7,7 +7,7 @@ New-NetNAT -Name NATNetwork -InternalIPInterfaceAddressPrefix 10.90.90.0/24
 #Create additional switch named "NATSwitch"
 New-VMSwitch -name NATSwitch -SwitchType Internal
 
-#You need to now the interface index. Run this command below:
+#You need to know the interface index. Run this command below:
 #Get-NetAdapter
 
 #Assign IP Address to NATSwitch
