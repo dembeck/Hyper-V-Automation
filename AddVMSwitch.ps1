e7dc6606d6eb1af2d446aeb58050c8de2eb50e28
Roden Hilario <rodjonh@gmail.com>
2021-02-23 05:53:47 +0000

    Edits to script

--- AddVMSwitch.ps1
+++ AddVMSwitch.ps1
@@ -1,5 +1,7 @@
+Get-NetIPAddress -AddressFamily IPv4 # Get the NetAdapterName to use
+
 #Add new Virtual Switch
-New-VMSwitch -name InternalSwitch -SwitchType Internal -AllowManagementOS $true
+New-VMSwitch -NetAdapterName "Ethernet" -Name "InternalSwitch" -SwitchType Internal -AllowManagementOS $true
 
 #Check status
 Get-VMSwitch * | Format-Table Name
